b29f4496c788e2d5671587138a5931a019024552
Test <m-mik@ro.ru>
2023-08-27 15:58:49 +0300

    done

--- react-app-example/src/components/toolbar/index.jsx
+++ react-app-example/src/components/toolbar/index.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 
-export default function Toolbar({ filters, onSelectFilter }) {
+export default function Toolbar({ filters, onSelectFilter, selected }) {
   return (
     <div className="toolbar-container">
       {filters.map((el) => {
         return (
           <div className="toolbar-item" key={uuidv4()}>
-            <span key={uuidv4()} onClick={onSelectFilter}>
-              {el}
-            </span>
+            {el === selected ? (
+              <span key={uuidv4()} onClick={onSelectFilter} className="select">
+                {el}
+              </span>
+            ) : (
+              <span key={uuidv4()} onClick={onSelectFilter}>
+                {el}
+              </span>
+             
+            )}
           </div>
         );
       })}
